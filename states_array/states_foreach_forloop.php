2e590beaf073c484518b676f4a0afe94e1adf00f
joseph park <jpark041@gmail.com>
2015-12-03 17:17:02 -0800

    initial commit

---
+++ states_array/states_foreach_forloop.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Display a dropdown menu in HTML (using select tag and option tag) that displays the different states. 
+Create this dropdown menu using for loops. Create another identical dropdown menu but, this time, use foreach statement.
+Once you're done with above exercise, insert three new states in the array $states: 'NJ', 'NY', 'DE'. 
+Display a new dropdown menu with the eight unique states.
+
+Your output should have three dropdown menus.
+*/
+
+
+
+?>
