4a5a40bcdf777dff17292f1feff42771254dd23b
joseph park <jpark041@gmail.com>
2015-12-03 17:05:15 -0800

    final commit

--- score_and_grade/score_and_grade.php
+++ score_and_grade/score_and_grade.php
@@ -14,26 +14,25 @@ Your grade is B.
 IMPORTANT: After you make this work, use a for loop to generate your score/grade 100 times.
  In other words, after you run your PHP script, it should display the score and grade 100 times.
 */
-$numb = rand(50,100);
 
-	for($i = 1; $i <= 100; $i++){
-	
-		if($numb < 70){
-			echo "<h1>Your Score is $numb.</h1>";
-			echo "<h2>Your Grade is D</h2>";
-		}
-		if($numb == 70 && $numb <=79){
-			echo "<h1>Your Score is $numb.</h1>";
-			echo "<h2>Your Grade is C</h2>";
-		}
-		if($numb == 80 && $numb <=89){
-			echo "<h1>Your Score is $numb.</h1>";
-			echo "<h2>Your Grade is B</h2>";
-		}
-		if($numb == 90 && $numb <= 100){
-			echo "<h1>Your Score is $numb.</h1>";
-			echo "<h2>Your Grade is A</h2>";
-		}
-	$numb;
+for($i = 1; $i <= 100; $i++){
+	$numb = rand(50,100);
+	echo "<h1>Your Score is: $numb.</h1><h2>Your Grade is: ";
+
+	if($numb < 70){
+		echo "D";
+	}
+	elseif($numb <=79){
+		echo "C";
+	}
+	elseif($numb <=89){
+		echo "B";
+	}
+	elseif($numb <= 100){
+		echo "A";
+	}
+	echo "</h2>";
+$numb;
 }
+
 ?> 
