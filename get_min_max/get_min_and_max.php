0135a304f7f6c54d39e3aa508a79cfa4b775ac3f
joseph park <jpark041@gmail.com>
2015-12-04 07:27:58 -0800

    directions added

--- get_min_max/get_min_and_max.php
+++ get_min_max/get_min_and_max.php
@@ -1,3 +1,18 @@
 <?php
+/*
+Create a function get_max_and_min() that takes an array of numbers and, then,
+returns both the minimum and the maximum number from the given array as an associative array. 
+Do not use the PHP function max() or min() to get this to work. 
+See if you can do this using arrays and for loops! For example:
+
+$sample = array( 135, 2.4, 2.67, 1.23, 332, 2, 1.02); 
+$output = get_max_and_min($sample); 
+var_dump($output); 
+//$output should be equal to array('max' => 332, 'min' => 1.02);
+*/
+
+
+
+
 
 ?>
