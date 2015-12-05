fed6d089b65773f3172458458f15da056dc85a70
joseph park <jpark041@gmail.com>
2015-12-04 21:45:23 -0800

    directions loaded

--- html_tables/html_tables.php
+++ html_tables/html_tables.php
@@ -1,4 +1,12 @@
 <?php 
+// $users = array( 
+//    array('first_name' => 'Michael', 'last_name' => 'Choi'),
+//    array('first_name' => 'John', 'last_name' => 'Supsupin'),
+//    array('first_name' => 'Mark', 'last_name' => 'Guillen'),
+//    array('first_name' => 'KB', 'last_name' => 'Tonel') 
+// );
+// Create a program that outputs a beautiful HTML table like this:
+
 
 
 ?>
