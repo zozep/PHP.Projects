5b170ef3920ff7f98f6063c8e47b40f8a892a9ef
joseph park <jpark041@gmail.com>
2015-12-04 20:33:42 -0800

    final commit

--- multiplication-table/multiplication_table.php
+++ multiplication-table/multiplication_table.php
@@ -8,7 +8,54 @@ Spend up to 5-10 minutes to make this look pretty using CSS!
 */
 
 
-
-
-
 ?>
+<html>
+<head>
+	<title>multiplication talbe</title>
+	<style type="text/css">
+		* {
+			padding: 0px auto;
+			margin: 0px auto;
+			border: 0px;
+		}
+		table {
+			border-collapse: collapse;
+		}
+		td, th {
+			border: 1px solid black;
+			width: 30px;
+			height: 30px;
+			text-align: center;
+		}
+		tr:nth-child(odd) td:not(:first-child) {
+			background-color: #c6c6c6;
+		}
+		th, strong {
+			font-size: 20px;
+		}
+	</style>
+</head>
+<body>
+	<table>
+		<thead>
+			<th></th>
+			<?php
+				for ($i=1; $i <= 9; $i++){
+					echo "<th>{$i}</th>";
+				}
+			?>
+		</thead>
+		<tbody>
+			<?php
+				for ($i = 1; $i <=9; $i++){
+					echo "<tr><td><strong>{$i}</strong></td>";
+					for($j = 1; $j <=9; $j++){
+						echo "<td>" . $i*$j . "</td>";
+					}
+					echo "</tr>";
+				}
+			?>
+		</tbody>			
+	</table>
+</body>
+</html>
--- ninjagold/index.php
+++ ninjagold/index.php
@@ -1,7 +1,6 @@
 <html>
 <head>
 	<title>Ninja Gold</title>
-	<link rel="styleesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="container">
@@ -10,26 +9,24 @@
 
 	<div class="buildings">
 		<h2>Farm</h2>
-		<p>(earn 10-20 gold)</p>
-		<form action="process.php" method="post">
-			<input type="submit" value="find gold!">
-			<input type="submit" value="find gold!">
-
-		</form>
-	</div>
-
-	<div class="buildings">
-		<h2>Cave</h2>
-		<p>(earn 5-10 gold)</p>
-		<form action="process.php" method="post">
+		<p>(earn gold)</p>
+		<form acgion="process.php" method="post">
 			<input type="submit" value="find gold!">
 		</form>
 	</div>
 
+<div class="buildings">
+		<h2>Farm</h2>
+		<p>(earn gold)</p>
+		<form acgion="process.php" method="post">
+			<input type="submit" value="find gold!">
+		</form>
+	</div>
+
 	<div class="buildings">
-		<h2>House</h2>
-		<p>(earn 2-5 gold)</p>
-		<form action="process.php" method="post">
+		<h2>Farm</h2>
+		<p>(earn gold)</p>
+		<form acgion="process.php" method="post">
 			<input type="submit" value="find gold!">
 		</form>
 	</div>
