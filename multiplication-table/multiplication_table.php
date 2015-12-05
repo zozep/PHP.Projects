38eb1949d1f13ae053d6e680ff8647285745c648
joseph park <jpark041@gmail.com>
2015-12-04 18:34:11 -0800

    initial commit

---
+++ multiplication-table/multiplication_table.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Create a program that displays a 9x9 multiplication table. 
+Display every other row in different background color, with one row in light grey color, the other row in white color.
+Make the font of the first row larger and make it bold. Similarly, style the first column so the font is larger and bold.
+Spend up to 5-10 minutes to make this look pretty using CSS!
+
+*/
+
+
+
+
+
+?>
