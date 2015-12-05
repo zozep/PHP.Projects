3f3ad836942c9e580829e75edf64296981af551e
joseph park <jpark041@gmail.com>
2015-12-04 17:05:20 -0800

    final commit

--- get_min_max/get_min_and_max.php
+++ get_min_max/get_min_and_max.php
@@ -8,11 +8,30 @@ See if you can do this using arrays and for loops! For example:
 $sample = array( 135, 2.4, 2.67, 1.23, 332, 2, 1.02); 
 $output = get_max_and_min($sample); 
 var_dump($output); 
-//$output should be equal to array('max' => 332, 'min' => 1.02);
+$output should be equal to array('max' => 332, 'min' => 1.02);
 */
+// $sample = array(135, 2.4, 2.67, 1.23, 332, 1.02);
+// $output = get_max_and_min($sample);
 
-
-
-
-
+function get_max_and_min($array){
+	$max = $array[0];
+	$min = $array[0];
+ 
+	for($i = 0; $i < count($array); $i++) {
+		if($array[$i]>$max)
+		{
+			$max = $array[$i];
+		}
+		elseif($array[$i] < $min)
+		{
+			$min = $array[$i];
+		}
+	}
+	$new_array = array("max" => $max, "min" => $min);
+	var_dump($new_array);
+}
+$array1 = array(56733, 8, 0, -5, 100000);
+$array_to = array(1,2,3,4,5,6);
+get_max_and_min($array_to);
+get_max_and_min($array1);
 ?>
