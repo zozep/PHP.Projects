840ca61d54a0db373083a96dd597049d55f5abb3
joseph park <jpark041@gmail.com>
2015-12-09 20:55:40 -0800

    added skeletons for testing

---
+++ simple test/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Survey Form PHP</title>
+        <meta name="description" content="This is a survey Form">
+    </head>
+
+<body>
+
+</body>
+</html>
