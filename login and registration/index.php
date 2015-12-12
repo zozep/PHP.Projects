82d150f3a7249ad5ea3e014ab1d47249d7854681
joseph park <jpark041@gmail.com>
2015-12-11 18:04:20 -0800

    initial commit - skeleton added

---
+++ login and registration/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>login and registration</title>
+</head>
+<body>
+	<form action="process.php" method="post">
+		<input type="email" name="email" placeholder="email">
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
