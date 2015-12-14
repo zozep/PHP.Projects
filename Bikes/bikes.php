b4c222589d29a7e6f1b3459d5ead7f697771921d
joseph park <jpark041@gmail.com>
2015-12-13 21:52:19 -0800

    display_info fx added

--- Bikes/bikes.php
+++ Bikes/bikes.php
@@ -8,34 +8,29 @@ Class Bike{
 		$this->max_speed = $max_speed;
 		$this->miles = 0;
 	}
-}
-
-$bmx = new Bike(250, "20mph");
-echo $bmx-> price . "<br>"; //250 
-echo $bmx-> max_speed. "<br>"; //20mph
-echo $bmx-> miles. "<br>"; //0
 
 	public function display_info(){
-		echo "Price: " . $this->price. "<br>";
-		echo "Maximum Speed: ".$this->max_speed. "<br>";
-		echo "Miles ridden: ".$this->miles. "<br><br>";
-	}
- 
-	public function drive(){
-		$this-> miles+=10;
-		echo "Driving" . $this->miles. "<br>";
-		return $this;
-	}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  kkkkkkkk                                                                                                                                                                    fffof                                             ffffffffooo
-	public function reverse(){
-		$this->miles-=5;
-		echo "Reversing" . $this->miles. "<br>";
-		return $this;
-		if($this->miles<5){
-			$this->miles=0;
-		}
-		return $this;
+		echo "Price: " . $this->price. "<br>" .
+		 	 "Max Speed: ".$this->max_speed. "<br>" .
+		 	 "Miles ridden: ".$this->miles. "<br><br>";
 	}
 }
+	// public function drive(){
+	// 	$this-> miles+=10;
+	// 	echo "Driving" . $this->miles. "<br>";
+	// 	return $this;
+	// }
+ //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  kkkkkkkk                                                                                                                                                                    fffof                                             ffffffffooo
+	// public function reverse(){
+	// 	$this->miles-=5;
+	// 	echo "Reversing" . $this->miles. "<br>";
+	// 	return $this;
+	// 	if($this->miles<5){
+	// 		$this->miles=0;
+	// 	}
+	// 	return $this;
+	// }
 
+$bmx = new Bike(250, "20mph");
+$bmx->display_info();
 ?>
