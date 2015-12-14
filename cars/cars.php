6d1ccb007f215e7e067211fa136ae1ed6b4cb5db
joseph park <jpark041@gmail.com>
2015-12-13 23:02:32 -0800

    price,speed,fuel,mileage, tax fx added. final commit

--- cars/cars.php
+++ cars/cars.php
@@ -1,41 +1,40 @@
 <?php
 
 Class Car{
-	public $price, $speed, $fuel, $mileage;
+	public $price, $speed, $fuel, $mileage, $tax;
 
-	// public function __construct($price, $max_speed){
-	// 	$this -> price = $price;
-	// 	$this -> max_speed = $max_speed;
-	// 	$this -> miles = 0;
+	public function __construct($price, $speed, $fuel, $mileage) {
+		$this -> price = $price;
+		$this -> speed = $speed;
+		$this -> fuel =  $fuel;
+		$this -> mileage = $mileage;
+		$this -> tax = ($this -> price > 10000) ? "0.15" : "0.12";
+
+		$this -> display_all();
 	}
 
-// 	public function display_info(){
-// 		echo "Price: " . $this->price. "<br>" .
-// 		 	 "Max Speed: ".$this->max_speed. "<br>" .
-// 		 	 "Miles ridden: ".$this->miles. "<br><br>";
-// 	}
+	public function display_all() {
+		foreach($this as $key => $value) {
+			echo $key . " : " . $value . "<br>";
+		}
+	}
+}
 
-// 	public function drive(){
-// 		echo "Driving" . "<br>" . $this->miles. "<br>";
-// 		$this -> miles += 10;
-// 		return $this;
-// 	}
+echo "<h4>Car 1: </h4>";
+$car1 = new Car(2000, "35 mph", "Full", "15 mpg");
 
-// 	public function reverse(){
-// 		echo "Reversing" . "<br>" . $this->miles . "<br>";
+echo "<h4>Car 2: </h4>";
+$car2 = new Car(2000, "5 mph", "Not Full", "25 mpg");
 
-// 		if($this-> miles < 6){
-// 			$this-> miles = 0;
-// 		}else{
-// 			$this-> miles -= 5;
-// 		}
-// 	}
-// }
-// $bmx = new Bike(200, "25 mph");
-// $bmx->drive();
-// $bmx->drive();
-// $bmx->drive();
-// $bmx->reverse();
-// $bmx->display_info();
+echo "<h4>Car 3: </h4>";
+$car3 = new Car(2000, "15 mph", "Full", "105 mpg");
 
+echo "<h4>Car 4: </h4>";
+$car4 = new Car(2000, "25 mph", "Kind of Full", "25 mpg");
+
+echo "<h4>Car 5: </h4>";
+$car5 = new Car(2000, "45 mph", "Empty", "25 mpg");
+
+echo "<h4>Car 6: </h4>";
+$car6 = new Car(20000000, "35 mph", "Empty", "15 mpg");
 ?>
