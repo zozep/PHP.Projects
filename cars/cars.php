f0a94059f1e2e759571d4ee7caa45d35365a0454
joseph park <jpark041@gmail.com>
2015-12-13 22:32:37 -0800

    initial commit - price,speed,fuel,mileage fx need to be added

---
+++ cars/cars.php
@@ -0,0 +1,41 @@
+<?php
+
+Class Car{
+	public $price, $speed, $fuel, $mileage;
+
+	// public function __construct($price, $max_speed){
+	// 	$this -> price = $price;
+	// 	$this -> max_speed = $max_speed;
+	// 	$this -> miles = 0;
+	}
+
+// 	public function display_info(){
+// 		echo "Price: " . $this->price. "<br>" .
+// 		 	 "Max Speed: ".$this->max_speed. "<br>" .
+// 		 	 "Miles ridden: ".$this->miles. "<br><br>";
+// 	}
+
+// 	public function drive(){
+// 		echo "Driving" . "<br>" . $this->miles. "<br>";
+// 		$this -> miles += 10;
+// 		return $this;
+// 	}
+
+// 	public function reverse(){
+// 		echo "Reversing" . "<br>" . $this->miles . "<br>";
+
+// 		if($this-> miles < 6){
+// 			$this-> miles = 0;
+// 		}else{
+// 			$this-> miles -= 5;
+// 		}
+// 	}
+// }
+// $bmx = new Bike(200, "25 mph");
+// $bmx->drive();
+// $bmx->drive();
+// $bmx->drive();
+// $bmx->reverse();
+// $bmx->display_info();
+
+?>
