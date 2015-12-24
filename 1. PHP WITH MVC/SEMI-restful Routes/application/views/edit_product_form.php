aee863c2aa93dc04295422fc4bb121b887dfb7d2
joseph park <jpark041@gmail.com>
2015-12-23 21:35:36 -0800

    form error - id needs to be defined

--- 1. PHP WITH MVC/SEMI-restful Routes/application/views/edit_product_form.php
+++ 1. PHP WITH MVC/SEMI-restful Routes/application/views/edit_product_form.php
@@ -4,12 +4,14 @@
 </head>
 <body>
 	<form method="post" action="<?= base_url("products/update")?>" <br>
+		<input ="hidden" name="<?= $id ?>">
 		<label>Name:</label> <input name="name"><br>
 		<label> Description:</label> <input name="description"><br>
 		<label> Price:</label> <input name="price"><br>
 		<br>
 		<input type="submit" value="Update">
 	</form>
+		<tbody>
 	<a href="/products">Go back</a>
 </body>
 </html>
